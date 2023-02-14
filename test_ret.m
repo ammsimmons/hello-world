643bc8932b75f3dc5ccb5f9c26f8a7807c6a8dca
Aaron Matthew Simmons <amsimmons@ucdavis.edu>
2023-02-14 12:39:10 -0800

    Create test_ret.m
    ret added to deleted local branch?

---
+++ test_ret.m
@@ -0,0 +1,11 @@
+% Prompt the user for input
+input_text = input('Enter some text: ', 's');
+
+% Capitalize the text
+capitalized_text = upper(input_text);
+
+% Display the result
+fprintf('Capitalized text: %s\n', capitalized_text);
+
+
+%added into the main branch??
