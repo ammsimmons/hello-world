581adc6b82309956a307c20ed4528095ddfb1803
Aaron Matthew Simmons <amsimmons@ucdavis.edu>
2023-02-16 14:46:56 -0800

    Added new scripts
    commit #1

--- test_weight_lbs.m
+++ test_weight_lbs.m
@@ -3,16 +3,16 @@ g_earth = 9.81; % acceleration due to gravity on Earth (m/s^2)
 g_moon = 1.62; % acceleration due to gravity on the Moon (m/s^2)
 g_mars = 3.71; % acceleration due to gravity on Mars (m/s^2)
 
-% Input weight in kilograms
-weight_kg = input('Enter your weight in kilograms: ');
+% Input weight in pounds
+weight_lbs = input('Enter your weight in pounds: ');
+
+% Convert weight from pounds to kilograms
+weight_kg = weight_lbs / 2.20462;
 
 % Calculate weight on the Moon and Mars
 weight_moon = weight_kg * g_moon / g_earth;
 weight_mars = weight_kg * g_mars / g_earth;
 
 % Output results
-fprintf('Your weight on the Moon is %.2f kilograms.\n', weight_moon);
-fprintf('Your weight on Mars is %.2f kilograms.\n', weight_mars);
-
-%% add new stuff to this
-%% I think i understand what I need to do now
+fprintf('Your weight on the Moon is %.2f pounds.\n', weight_moon * 2.20462);
+fprintf('Your weight on Mars is %.2f pounds.\n', weight_mars * 2.20462);
